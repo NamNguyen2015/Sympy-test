478af2190758bc29a813d53ad787fe7e190ee2ee
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-03-31 10:34:35 +0200

    e

--- Streamlit_Wind_Actions_def.py
+++ Streamlit_Wind_Actions_def.py
@@ -160,7 +160,7 @@ st.write('the turbulence divided be the mean velocity, and is expressed by the f
 # In[5]:
 st.write('Input parameters')
 
-c_dir_val = st.number_input("Enter value for {}".format(latex(r'$c_{dir}$')), value=1.0)
+c_dir_val = st.number_input("Enter value for {}".format(st.latex(r'$c_{dir}$')), value=1.0)
 
 c_season_val=1.0
 v_b0_val= 26
